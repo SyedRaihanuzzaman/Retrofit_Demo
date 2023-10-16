c61984bdcaadfc4bf99b7c2406ffce038ec6b1f5
Syed Raihanuzzaman <syed15-10568@diu.edu.bd>
2023-10-16 10:32:03 +0600

    first commit

---
+++ app/src/main/java/com/raihan/retrofitdemo/QuotesApi.kt
@@ -0,0 +1,9 @@
+package com.raihan.retrofitdemo
+
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface QuotesApi {
+    @GET("/quotes")
+    suspend fun getQuotes() : Response<QuoteList>
+}
